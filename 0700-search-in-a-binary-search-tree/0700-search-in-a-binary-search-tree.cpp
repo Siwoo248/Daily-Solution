f0937fbe9195aa2fd0aefa5ea11834040a3cf5e1
Si Woo Lee <siwoo020408@gmail.com>
2026-02-20 12:48:53 +0100

    Time: 0 ms (100%), Space: 35.4 MB (62.56%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
@@ -9,22 +9,41 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution {             // BST-Binary Search Tree is in order
 public:
-    TreeNode* searchBST(TreeNode* root, int val) {
-        if(root == nullptr) return nullptr;
-        
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while(!q.empty()){
-                TreeNode* cur = q.front();
-                if(cur->val == val) return cur;
-                q.pop();
-
-                if(cur->left) q.push(cur->left);
-                if(cur->right) q.push(cur->right);
+    TreeNode* searchBST(TreeNode* root, int val){   // Using binary search method
+        TreeNode* cur = root;
+        while(cur != nullptr){
+            if(cur->val == val) return cur;         // Comparing with current node
+            else if(cur->val > val){                // if current is smaller -> go right
+                cur = cur->left;                    // if current is larger -> go left
+            }
+            else{
+                cur = cur->right;
+            }
         }
         return nullptr;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     TreeNode* searchBST(TreeNode* root, int val) {  // Using BFS method
+//         if(root == nullptr) return nullptr;
+        
+//         queue<TreeNode*> q;
+//         q.push(root);
+
+//         while(!q.empty()){
+//                 TreeNode* cur = q.front();
+//                 if(cur->val == val) return cur;
+//                 q.pop();
+
+//                 if(cur->left) q.push(cur->left);
+//                 if(cur->right) q.push(cur->right);
+//         }
+//         return nullptr;
+//     }
+// };
